db8af8b6748cb2ea96bba3f9733028bdfdd228ab
Swastik Biswas <swastikbiswas962@gmail.com>
2022-12-20 13:37:47 +0530

    added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { ThemeProvider } from 'next-themes'
+
 import '../styles/globals.css'
+import Navbar from '../components/global/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class'>
+        <Navbar />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
