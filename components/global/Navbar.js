21110a97da19b0529523f7df79b5044b2acf1dc7
Swastik Biswas <swastikbiswas962@gmail.com>
2022-12-22 12:55:11 +0530

    added theme toggle

--- components/global/Navbar.js
+++ components/global/Navbar.js
@@ -1,4 +1,19 @@
 import { useState } from 'react'
+import { useTheme } from 'next-themes'
+import { SunIcon, MoonIcon } from '@heroicons/react/20/solid'
+
+const ThemeChanger = () => {
+    const { theme, setTheme } = useTheme()
+    return (
+        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'light' ? (
+                <SunIcon className="w-6 h-6 mr-4 text-neutral-900" />
+            ) : (
+                <MoonIcon className="w-5 h-5 mr-4 text-neutral-100" />
+            )}
+        </button>
+    )
+}
 
 function NavLink({to, children}) {
     return <a href={to} className={`mx-4`}>
@@ -29,17 +44,18 @@ function Navbar() {
 
     return (
         <nav className="flex filter drop-shadow-md bg-neutral-50 dark:bg-neutral-900 px-4 py-4 h-20 items-center">
-            <MobileNav open={open} setOpen={setOpen}/>
+            <MobileNav open={open} setOpen={setOpen} />
             <div className="w-3/12 flex items-center">
                 <a className="text-2xl text-neutral-900 dark:text-neutral-50 font-semibold" href="/">MedStreet</a>
             </div>
             <div className="w-9/12 flex justify-end items-center">
-                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+                <ThemeChanger />
+                <div className="z-50 flex relative w-6 h-6 flex-col justify-between items-center md:hidden" onClick={() => {
                     setOpen(!open)
                 }}>
-                    <span className={`h-1 w-full bg-neutral-900 dark:bg-neutral-50 rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-neutral-900 dark:bg-neutral-50 rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-2.5" : ""}`} />
                     <span className={`h-1 w-full bg-neutral-900 dark:bg-neutral-50 rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-                    <span className={`h-1 w-full bg-neutral-900 dark:bg-neutral-50 rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-neutral-900 dark:bg-neutral-50 rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
                 </div>
                 <div className="hidden md:flex justify-center text-neutral-900 dark:text-neutral-50">
                     <NavLink to="/solutions">
