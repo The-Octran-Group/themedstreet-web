e8f1b868b76be62b4d7940a240397802c8c7fc90
Swastik Biswas <swastikbiswas962@gmail.com>
2022-12-22 12:31:45 +0530

    fixed keywords

--- components/global/Seo.js
+++ components/global/Seo.js
@@ -7,7 +7,7 @@ function Seo({ title, description, robots }) {
             <meta name="description" content={description} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
             <meta name="robots" content={robots} />
-            <meta charset="UTF-8" />
+            <meta charSet="UTF-8" />
             <link rel="icon" href="/favicon.ico" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
